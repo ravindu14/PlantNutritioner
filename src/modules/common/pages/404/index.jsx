5bfa9cb3e8d0181ce163dfbf5d8d9c8528ab319e
ravindu14 <ravindulandekumbura14@gmail.com>
2020-10-25 20:30:47 +0530

    :bug: resolve common pages

--- src/modules/common/pages/404/index.jsx
+++ src/modules/common/pages/404/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "components/mainLayout";
+import Layout from "components/layout";
 import notFound from "assets/image/404.svg";
 
 import "./styles.scss";
